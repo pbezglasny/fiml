225f2aa06fbfdade8db8fa51efff146f88782fe7
Pavel Bezglasnyy <pavelb23@gmail.com>
2026-05-27 12:42:02 +0200

    feat(bench): added benches for sma and ring buffer

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,16 @@
 mod indicators;
 mod ring_buffer;
 mod types;
+mod vectors;
 
 use std::{error::Error, fmt::Display};
 
 pub use indicators::averages::SimpleMovingAverage;
-pub use ring_buffer::{HeapRingBuffer, RingBuffer, StackRingBuffer};
+pub use ring_buffer::{
+    HeapRingBuffer, RingBuffer, StackRingBuffer, new_heap_ring_buffer, new_stack_ring_buffer,
+};
 pub use types::Float;
+pub use vectors::{FeatureVector, Handler};
 
 pub type Result<T> = std::result::Result<T, FimlError>;
 
@@ -15,12 +19,14 @@ pub type Result<T> = std::result::Result<T, FimlError>;
 #[non_exhaustive]
 pub enum FimlError {
     InvalidArgument(String),
+    FeatureVectorFull,
 }
 
 impl Display for FimlError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             FimlError::InvalidArgument(msg) => write!(f, "invalid argument: {}", msg),
+            FimlError::FeatureVectorFull => write!(f, "feature vector is full"),
         }
     }
 }
